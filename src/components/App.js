ddf4405a413048b4cbc0f65296ad2b0cb1f8446b
Amine_Zerradi <dasileker@gmail.com>
2021-06-25 10:34:50 +0100

    add the componets, actions, action-types folders + index.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="header">
+        <h1>Book-store-cms</h1>
+      </div>
+    </>
+
+  );
+}
+
+export default App;
